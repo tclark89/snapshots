de4109a54b83504474c220157f0925d9ecb23d97
Tyler Clark <tylerclark2@gmail.com>
2023-06-10 06:41:45 -0500

    Updates for Virtual Machines

--- snapshots.sh
+++ snapshots.sh
@@ -4,9 +4,12 @@ timeStamp=$(date '+%Y-%m-%d_%H-%M-%S')
 homeSnapName=home-${timeStamp}
 rootSnapName=root-${timeStamp}
 plexSnapName=plex-${timeStamp}
+libVirtSnapName=libvirt-images-${timeStamp}
+vms_snap_name=vms_${timeStamp}
 publicSnapName=public-${timeStamp}
 tylerSnapName=tyler-${timeStamp}
 meaganSnapName=meagan-${timeStamp}
+machinesSnapName=machines-${timeStamp}
 
 snapDir=/mnt/snapshots
 raidDir=/mnt/RAID
@@ -21,11 +24,15 @@ export XZ_DEFAULTS="-T 3"
 btrfs subvolume snapshot -r /home ${snapDir}/home-current
 btrfs subvolume snapshot -r / ${snapDir}/root-current
 btrfs subvolume snapshot -r /mnt/diskRoot/plexmediaserver/ ${snapDir}/plex-current
+# btrfs subvolume snapshot -r /mnt/diskRoot/libvirt-images/ ${snapDir}/libvirt-images-current
+btrfs subvolume snapshot -r /home/tyler/vms/ ${snapDir}/vms
+btrfs subvolume snapshot -r /var/lib/machines/ ${snapDir}/machines-current
 
 # Also snapshot as dated
 btrfs subvolume snapshot -r ${snapDir}/home-current ${snapDir}/${homeSnapName}
 btrfs subvolume snapshot -r ${snapDir}/root-current ${snapDir}/${rootSnapName}
 btrfs subvolume snapshot -r ${snapDir}/plex-current ${snapDir}/${plexSnapName}
+btrfs subvolume snapshot -r ${snapDir}/machines-current ${snapDir}/${machinesSnapName}
 
 # RAID snapshots. Bad Idea
 #btrfs subvolume snapshot -r ${raidDir}/public ${raidSnapDir}/${publicSnapName}
@@ -56,6 +63,14 @@ tar \
 	${plexSnapName}
 mv ${snapDir}/${plexSnapName}.tar.xz ${backupDir}/
 
+echo "Creating machines Tarball..."
+tar \
+	-caf ${snapDir}/${machinesSnapName}.tar.xz \
+	--checkpoint=1000 \
+	--checkpoint-action=echo"Machines: #%u: %T" \
+	${machinesSnapName}
+mv ${snapDir}/${machinesSnapName}.tar.xz ${backupDir}/
+
 echo "Creating tyler Tarball..."
 cd ${homeSnapName}
 tar \
@@ -68,9 +83,21 @@ mv ${snapDir}/${tylerSnapName}.tar.xz ${backupDir}/
 echo "Creating meagan Tarball..."
 tar -caf ${snapDir}/${meaganSnapName}.tar.xz meagan
 mv ${snapDir}/${meaganSnapName}.tar.xz ${backupDir}/
- 
+
+
+#echo "Rsyncing libvirt images..."
+#rsync -avh --progress ${snapDir}/libvirt-images-current/ ${raidDir}/VMs/libvirt-images/
+#chown -R tyler ${raidDir}/VMs/
+#chgrp -R tyler ${raidDir}/VMs/
+
+echo "Rsyncing VM images..."
+rsync -avh --progress ${snapDir}/vms/ ${raidDir}/VMs/
+chown -R tyler ${raidDir}/VMs/
+chgrp -R tyler ${raidDir}/VMs/
 
 # Delete current backup
 btrfs subvolume delete ${snapDir}/home-current
 btrfs subvolume delete ${snapDir}/root-current
 btrfs subvolume delete ${snapDir}/plex-current
+#btrfs subvolume delete ${snapDir}/libvirt-images-current
+btrfs subvolume delete ${snapDir}/vms
